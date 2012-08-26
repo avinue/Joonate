ae506d1e3c3ed72bbe0dfe5b2b422b91f84b75fe
Avi <tech@agnvig.dk>
2012-08-26 22:53:59 +0200

    New

--- Helper/CreateFeed.php
+++ Helper/CreateFeed.php
@@ -3,5 +3,9 @@ require_once(dirname(__DIR__) . '/Config/InitPHP.php');
 
 $ctrFeeds = new ControllerFeeds();
 
-
+if (!isset($_POST['submit'])) {  
+    header('location:../index');
+} else {
+    $ctrFeeds->checkLogin($_POST['feedTitle'], $_POST['feedInfo']);
+}
 ?>
---
+++ View/memberPanel.php
@@ -0,0 +1,17 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title></title>
+    </head>
+    <body>
+        <div class="memberPanelFeed">
+            <form action="Helper/CreateFeed.php" method="post">
+                <div>Title</div>
+                <div><input type="text" name="feedTitle" /></div>
+                <div>Information</div>
+                <div><textarea type="text" name="feedInfo"></textarea></div>
+                <div><input type="submit" name="submit" value="Create"/></div>
+            </form>
+        </div>
+    </body>
+</html>
