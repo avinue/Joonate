75ca42b536c9aa9f3c2d01b59641bf30121856d0
Avi <tech@agnvig.dk>
2012-09-04 19:47:13 +0200

    what

--- Config/InitHTML.php
+++ Config/InitHTML.php
@@ -3,7 +3,7 @@
  * HTML adds
  */
 echo "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />";
-echo "<link rel=StyleSheet href='Content/css.css' type='text/css' />";
+echo "<link rel=StyleSheet href='Content/main.css' type='text/css' />";
 echo "<link rel=StyleSheet href='Content/feeds.css' type='text/css' />";
 echo "<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Capriola|Ropa+Sans|Advent+Pro|Poiret+One|Convergence|Permanent+Marker|Kameron|Judson' rel='stylesheet' type='text/css'>";
 echo "<script src='js/jquery-1.8.0.min.js' type='text/javascript'></script>";
